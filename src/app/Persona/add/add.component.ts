b1f54c141bacefe1ad8bcaf3ac511e97fc0f4f03
Miguel Ángel <miguelherc98@gmail.com>
2023-01-25 16:53:57 -0500

    Probando Spring Boot y Angular #1

---
+++ src/app/Persona/add/add.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Persona } from 'src/app/Modelo/Persona';
+import { ServiceService } from 'src/app/Service/service.service';
+
+@Component({
+  selector: 'app-add',
+  templateUrl: './add.component.html',
+  styleUrls: ['./add.component.css']
+})
+export class AddComponent implements OnInit {
+
+  persona:Persona=new Persona();
+  constructor(private router:Router, private service:ServiceService) { }
+
+  ngOnInit() {
+  }
+
+  Guardar(){
+    this.service.createPersona(this.persona)
+    .subscribe(data=>{
+      alert("Usuario agregrado con exito");
+      this.router.navigate(["listar"]);
+    })
+  }
+}
